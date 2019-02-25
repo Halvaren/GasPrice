53921c551896b26e3c1e60545e662b75b934c10c
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-02-25 17:48:32 +0100

    .

---
+++ app/src/main/java/alvaro/sabi/rosquilletas/gasprice/GasType.java
@@ -0,0 +1,20 @@
+package alvaro.sabi.rosquilletas.gasprice;
+
+public enum GasType {
+    G98("Gasoline 98", 3),
+    GOA("Diesel",4),
+    NGO("Premium Disel", 5),
+    G95("Gasoline 95", 15),
+    LPG("Liquefies petroleum gases", 17);
+
+    private final String gasName;
+    private final int code;
+
+    GasType(String gasName, int code) {
+        this.gasName = gasName;
+        this.code = code;
+    }
+    private String gasName() {return gasName;}
+    private int code(){return code;}
+
+}
