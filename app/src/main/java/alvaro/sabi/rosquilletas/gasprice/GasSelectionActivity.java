6506a9df1cdd0a77e98a3dab618fe524265ebd9c
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-02-25 17:45:57 +0100

    Configuracion inicial terminada

--- app/src/main/java/alvaro/sabi/rosquilletas/gasprice/GasSelectionActivity.java
+++ app/src/main/java/alvaro/sabi/rosquilletas/gasprice/GasSelectionActivity.java
@@ -1,7 +1,8 @@
 package alvaro.sabi.rosquilletas.gasprice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GasSelectionActivity extends AppCompatActivity {
 
