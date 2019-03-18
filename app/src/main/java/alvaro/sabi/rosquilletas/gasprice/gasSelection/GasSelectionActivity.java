4a06e31355918506f9793362538bf986433e840c
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2019-03-18 18:53:27 +0100

    Más comentarios (falta comentar el model, presenter y view de la segunda actividad, el dialog, el gas queries y el gas type y también el station price)

--- app/src/main/java/alvaro/sabi/rosquilletas/gasprice/gasSelection/GasSelectionActivity.java
+++ app/src/main/java/alvaro/sabi/rosquilletas/gasprice/gasSelection/GasSelectionActivity.java
@@ -30,13 +30,15 @@ public class GasSelectionActivity extends AppCompatActivity {
 
     private GasSelectionPresenter presenter;
 
+    //
+    // Elementos del view
+    //
+
     private Spinner communitySpinner; //Spinner que muestra las comunidades
     private Spinner provinceSpinner; //Spinner que muestra las provincias de la comunidad seleccionada
     private AutoCompleteTextView townText; //Las opciones de autocompletado son los pueblos de la provincia seleccionada
     private Spinner fuelTypeSpinner; //Spinner que muestra los tipos de combustibles
-
     private Button showPricesButton; //Botón para pasar a la siguiente actividad
-
     private ProgressBar progressBar; //Barra de progreso radial que aparece mientras se rellenan los spinners (se hace consulta a la base de datos)
 
     @Override
@@ -110,17 +112,21 @@ public class GasSelectionActivity extends AppCompatActivity {
         getCommunities(); //Se piden al presenter las comunidades (están guardadas en la database)
     }
 
-    public void getCommunities()
+    //
+    // Métodos para rellenar los spinner y el texto de autocompletado
+    //
+
+    public void getCommunities() //Pide al presenter la lista de comunidades
     {
         presenter.getCommunities();
     }
 
-    public void getProvinces(Community community)
+    public void getProvinces(Community community) //Pide al presenter la lista de provincias de la comunidad seleccionada
     {
         presenter.getProvinces(community.id);
     }
 
-    public void getTowns(Province province)
+    public void getTowns(Province province) //Pide al presenter la lista de pueblos de la provincia seleccionada
     {
         presenter.getTowns(province.id);
     }
@@ -148,7 +154,7 @@ public class GasSelectionActivity extends AppCompatActivity {
         townText.setAdapter(adapter); //Se le asigna al texto de autocompletado un adapter que contiene la lista de pueblos
     }
 
-    private void showFuelTypes()
+    private void showFuelTypes() //Rellana el spinner de los combustibles con los combustibles guardados en memoria
     {
         ArrayList<GasType> list = new ArrayList(); //Se genera una lista con los tipos de combustibles
         list.add(GasType.G95);
@@ -162,9 +168,20 @@ public class GasSelectionActivity extends AppCompatActivity {
         fuelTypeSpinner.setAdapter(adapter); //Se le asigna al spinner un adapter que contiene la lista de combustibles
     }
 
-    private void onTownTextChanged(String s)
+    //
+    // Métodos relacionados con el campo de pueblos
+    //
+
+    private void setTownText(String value) //Cambia el texto del campo del pueblo
     {
-        presenter.onTownTextChanged(s); //Envia al presenter el string con el texto actual del campo del pueblo para que compruebe si el pueblo existe en la provincia seleccionada
+        townText.setText(value);
+        onTownTextChanged(townText.getText().toString()); //En todas las llamadas a este método, el valor pasado es una cadena vacía, por lo que esta sentencia no tiene utilidad
+        //Pero ello permite que si se pasara un valor distinto a una cadena vacía, comprobaría si el nuevo pueblo es correcto o no
+    }
+
+    private void onTownTextChanged(String s) //Envia al presenter el string con el texto actual del campo del pueblo para que compruebe si el pueblo existe en la provincia seleccionada
+    {
+        presenter.onTownTextChanged(s);
     }
 
     public void changedTownTextColor(boolean correct) //Este método será llamado por el presenter una vez haya comprobado si el pueblo existe en la provincia seleccionada
@@ -179,25 +196,13 @@ public class GasSelectionActivity extends AppCompatActivity {
         }
     }
 
+    //
+    // Otros métodos
+    //
+
     public void enableShowPricesButton(boolean param) //Activa/desactiva el botón para pasar a la siguiente actividad en función de si el pueblo introducido existe
     {
         showPricesButton.setEnabled(param);
-    }
-
-    private void setTownText(String value) //Cambia el texto del campo del pueblo
-    {
-        townText.setText(value);
-        onTownTextChanged(townText.getText().toString()); //En todas las llamadas a este método, el valor pasado es una cadena vacía, por lo que esta sentencia no tiene utilidad
-                                                          //Pero ello permite que si se pasara un valor distinto a una cadena vacía, comprobaría si el nuevo pueblo es correcto o no
-    }
-
-    public void nextActivity(View view) //Será llamado cuando se pulse el botón para pasar a la siguiente actividad
-    {
-        Intent intent = new Intent(GasSelectionActivity.this, ShowGasStationActivity.class);
-        intent.putExtra("Town", presenter.getSelectedTown()); //A la siguiente actividad es necesario para el pueblo seleccionado
-        intent.putExtra("GasType", ((GasType)fuelTypeSpinner.getSelectedItem())); //Y el combustible seleccionado
-
-        startActivity(intent);
     }
 
     public void showToast(String message) //Sirve mostrar mensajes de error (en concreto, errores de la database)
@@ -208,5 +213,14 @@ public class GasSelectionActivity extends AppCompatActivity {
     public void showProgressBar(boolean param) //Muestra y desmuestra la barra de progreso
     {
         progressBar.setVisibility(param ? View.VISIBLE : View.GONE);
+    }
+
+    public void nextActivity(View view) //Será llamado cuando se pulse el botón para pasar a la siguiente actividad
+    {
+        Intent intent = new Intent(GasSelectionActivity.this, ShowGasStationActivity.class);
+        intent.putExtra("Town", presenter.getSelectedTown()); //A la siguiente actividad es necesario para el pueblo seleccionado (se lo pide al presenter, que se lo pedirá al model)
+        intent.putExtra("GasType", ((GasType)fuelTypeSpinner.getSelectedItem())); //Y el combustible seleccionado
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/alvaro/sabi/rosquilletas/gasprice/gasSelection/GasSelectionPresenter.java
+++ app/src/main/java/alvaro/sabi/rosquilletas/gasprice/gasSelection/GasSelectionPresenter.java
@@ -15,90 +15,101 @@ public class GasSelectionPresenter {
     private GasSelectionActivity view;
     private Model model;
 
-    public GasSelectionPresenter(GasSelectionActivity view, Context context)
+    public GasSelectionPresenter(GasSelectionActivity view, Context context) //Constructor
     {
         this.view = view;
         model = Model.getInstance(context);
     }
 
-    public void getCommunities()
+    //
+    // Métodos para pedir y devolver las listas de comunidades, provincias y pueblos
+    //
+
+    public void getCommunities() //Se piden las comunidades
     {
-        view.showProgressBar(true);
+        view.showProgressBar(true); //Se muestra la barra de progreso y se desmuestrará cuando se envíe la lista de comunidades
         model.getCommunities(new Response.Listener<Community[]>() {
                                  @Override
-                                 public void onResponse(Community[] response) {
+                                 public void onResponse(Community[] response) { //Listener que llamará al método que devuelve la lista de comunidades a la view cuando se haya completado la query a la database
                                      showCommunities(response);
                                  }
                              }, new Response.Listener<String>() {
                                  @Override
-                                 public void onResponse(String response) {
+                                 public void onResponse(String response) { //Listener que servirá para mostrar que se ha producido un error en la query a la database (no se ha recibido nada o se ha recibido una lista vacía)
                                      showToast(response);
                                  }
                              }
                 , true);
     }
 
-    public void getProvinces(int communityID)
+    public void getProvinces(int communityID) //Se piden las provincias de la comunidad seleccionada
     {
-        view.showProgressBar(true);
+        view.showProgressBar(true); //Se muestra la barra de progreso y se desmuestrará cuando se envíe la lista de provincias
         model.getProvinces(new Response.Listener<Province[]>() {
                             @Override
-                            public void onResponse(Province[] response) {
+                            public void onResponse(Province[] response) { //Listener que llamará al método que devuelve la lista de provincias a la view cuando se haya completado la query a la database
                                 showProvinces(response);
                             }
                         }, new Response.Listener<String>() {
                             @Override
-                            public void onResponse(String response) {
+                            public void onResponse(String response) { //Listener que servirá para mostrar que se ha producido un error en la query a la database (no se ha recibido nada o se ha recibido una lista vacía)
                                 showToast(response);
                             }
                         }, communityID);
     }
 
-    public void getTowns(int provinceID)
+    public void getTowns(int provinceID) //Se piden los pueblos de las provincia seleccionada
     {
-        view.showProgressBar(true);
+        view.showProgressBar(true); //Se muestra la barra de progreso y se desmuestrará cuando se envíe la lista de pueblos
         model.getTowns(new Response.Listener<Town[]>() {
                         @Override
-                        public void onResponse(Town[] response) {
+                        public void onResponse(Town[] response) { //Listener que llamará al método que devuelve la lista de pueblos a la view cuando se haya completado la query a la database
                             showTowns(response);
                         }
                     }, new Response.Listener<String>() {
                         @Override
-                        public void onResponse(String response) {
+                        public void onResponse(String response) { //Listener que servirá para mostrar que se ha producido un error en la query a la database (no se ha recibido nada o se ha recibido una lista vacía)
                             showToast(response);
                         }
                     }, provinceID);
     }
 
-    public void showCommunities(Community[] list)
+    public void showCommunities(Community[] list) //Envia al view la lista de comunidades ya obtenida por el modelo
     {
-        view.showProgressBar(false);
+        view.showProgressBar(false); //Hace desaparecer la barra de progreso porque ya se tiene la lista de comunidades
         view.showCommunities(list);
     }
 
-    public void showProvinces(Province[] list)
+    public void showProvinces(Province[] list) //Envia al view la lista de provincias ya obtenida por el modelo
     {
-        view.showProgressBar(false);
+        view.showProgressBar(false); //Hace desaparecer la barra de progreso porque ya se tiene la lista de provincias
         view.showProvinces(list);
     }
 
-    public void showTowns(Town[] list)
+    public void showTowns(Town[] list) //Envia al view la lista de pueblos ya obtenida por el modelo
     {
-        view.showProgressBar(false);
+        view.showProgressBar(false); //Hace desaparecer la barra de progreso porque ya se tiene la lista de pueblos
         view.showTowns(list);
     }
 
-    public void onTownTextChanged(String s) {
+    //
+    // Otros métodos
+    //
+
+    public void onTownTextChanged(String s)  //Recibe un texto correspondiente a un posible pueblo. Le pide al modelo que verifique si existe y
+                                             //manda al resultado a la view para cambiar el color del texto del pueblo y activar/desactivar el botón de cambiar de actividad
+    {
         boolean correct = model.verifyTextChanged(s);
         view.changedTownTextColor(correct);
         view.enableShowPricesButton(correct);
     }
 
-    public Town getSelectedTown() {
+    public Town getSelectedTown() //Devuelve el pueblo seleccionado pidiéndoselo al modelo
+    {
         return model.getSelectedTown();
     }
 
-    public void showToast(String message)
+    public void showToast(String message) //Recibe un mensaje que mostrar mediante un Toast (el view generará el Toast)
     {
         view.showToast(message);
     }
